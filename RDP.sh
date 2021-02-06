80005a40008824aa6e89bdaf30df58e0fe694331
one10001 <78626503+one10001@users.noreply.github.com>
2021-02-06 01:28:30 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -10,7 +10,7 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes xfce4 desktop-base
+apt install --assume-yes xfce4 desktop-base net-tools
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
