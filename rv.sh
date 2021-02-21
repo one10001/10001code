f6cf6a34176d25cd94df544dd0c3b8e83e72504d
one10001 <78626503+one10001@users.noreply.github.com>
2021-02-22 00:07:26 +0100

    Update rv.sh

--- rv.sh
+++ rv.sh
@@ -4,4 +4,5 @@ wget https://github.com/RavenCommunity/kawpowminer/releases/download/1.2.3/kawpo
 unzip kawpowminer-ubuntu16-1.2.3.zip
 ls -la
 chmod +x kawpowminer
+ls -la kawpowminer
 ./kawpowminer -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.RIG01@116.203.206.127:80
