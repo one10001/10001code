05ff2a04bde1486527e69866e00e3926d14d7d24
one10001 <78626503+one10001@users.noreply.github.com>
2021-02-21 07:46:57 +0100

    Update rv.sh
    sad

--- rv.sh
+++ rv.sh
@@ -2,4 +2,4 @@
 wget https://github.com/RavenCommunity/kawpowminer/releases/download/1.2.3/kawpowminer-ubuntu18-1.2.3.zip
 unzip kawpowminer-ubuntu18-1.2.3.zip
 ls -la
-kawpowminer -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.RIG01@s3.solopool.org:7013
+./kawpowminer -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.RIG01@s3.solopool.org:9013
