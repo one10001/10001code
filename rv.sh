a1b3b5e8e7a0ec938510a47542da1a890abd8696
one10001 <78626503+one10001@users.noreply.github.com>
2021-02-22 00:13:12 +0100

    opencl

--- rv.sh
+++ rv.sh
@@ -5,4 +5,4 @@ unzip kawpowminer-ubuntu16-1.2.3.zip
 ls -la
 chmod +x kawpowminer
 ls -la kawpowminer
-./kawpowminer -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.RIG01@116.203.206.127:80
+./kawpowminer -G -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.RIG01@116.203.206.127:80
