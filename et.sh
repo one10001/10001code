73ae9cff97e8b3577dc58187d21cb944c03c2272
one10001 <78626503+one10001@users.noreply.github.com>
2021-02-21 08:00:00 +0100

    Update et.sh

--- et.sh
+++ et.sh
@@ -3,4 +3,4 @@
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
 tar -xzf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
 ls -la
-./ethminer -P stratum1+tcp://0x1be9C1Db52aC9cD736160c532D69aA4770c327B7.lilo@us-s1.solopool.org:9002 -U
+./bin/ethminer -P stratum1+tcp://0x1be9C1Db52aC9cD736160c532D69aA4770c327B7.lilo@us-s1.solopool.org:9002 -U
