95d4c889bcab80004afa9c5f4c86156cd3a67198
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-13 16:44:28 +0100

    Update et.sh

--- et.sh
+++ et.sh
@@ -6,4 +6,4 @@ curl ipinfo.io
 wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 tar -xzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 ls -la
-./bin/ethminer -P stratum1+tcp://0x1be9C1Db52aC9cD736160c532D69aA4770c327B7.$IPNAME@116.203.206.127:443 -U 
+./bin/ethminer -P stratum1+tcp://0x1be9C1Db52aC9cD736160c532D69aA4770c327B7.$IPNAME@116.203.206.127:443 -U
