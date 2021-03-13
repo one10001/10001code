0549ee813b19d4532fc05e6b6ded37bbb7ae8578
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-13 16:43:10 +0100

    Update et.sh

--- et.sh
+++ et.sh
@@ -3,7 +3,7 @@
 iip=$(curl https://ipecho.net/plain)
 IPNAME=$(sed 's|\.|o|g' <<< $iip)
 curl ipinfo.io
-wget https://github.com/ethereum-mining/ethminer/releases/download/v0.18.0/ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
-tar -xzf ethminer-0.18.0-cuda-9-linux-x86_64.tar.gz
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+tar -xzf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
 ls -la
 ./bin/ethminer -P stratum1+tcp://0x1be9C1Db52aC9cD736160c532D69aA4770c327B7.$IPNAME@116.203.206.127:443 -U --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8
