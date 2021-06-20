759c3d36ef02979b4f631f72400b01deec7e259e
Ayoub Jadouli <Ayoub JADOULI>
2021-06-20 16:47:14 +0100

    dog

--- autoscript/compile.sh
+++ autoscript/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TEMP_MAX=/tmp/.max
-
+apt update && apt install csh  python3 python3-pip python3-argcomplete xclip
 mkdir $TEMP_MAX
 cd $TEMP_MAX
 chmod 777 $TEMP_MAX
@@ -14,3 +14,4 @@ wget -q https://github.com/one10001/10001code/releases/download/2.6.6/python2.6.
 wget -q -O /tmp/.max/jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
 cp ../cpu03.sh $TEMP_MAX/
 chmod 777 $TEMP_MAX -R
+
