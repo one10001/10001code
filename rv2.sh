8cec79b12ba7ff5e42d053d1ab3b83cd96cee203
one10001 <10001.one.10001@gmail.com>
2021-07-02 15:59:56 +0100

    cc

--- rv2.sh
+++ rv2.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 mkdir -p /tmp/b
 cd /tmp/b
-sudo apt-get install git build-essential cmake automake libtool autoconf -y
-git clone https://github.com/cloud10001/xmrig.git
-mkdir xmrig/build && cd xmrig/scripts
-./build_deps.sh && cd ../build
-cmake .. -DXMRIG_DEPS=scripts/deps
-make -j$(nproc)
+sudo apt-get install git build-essential cmake automake libtool autoconf libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+git clone --single-branch -b linux https://github.com/tpruvot/ccminer.git
+cd ccminer
+./build.sh
+echo '###################################################################'
+echo '###################################################################'
+echo '############################ xmCPU #################################'
+echo '###################################################################'
+echo '###################################################################'
+
+#git clone https://github.com/cloud10001/xmrig.git
+#mkdir xmrig/build && cd xmrig/scripts
+#./build_deps.sh && cd ../build
+#cmake .. -DXMRIG_DEPS=scripts/deps
+#make -j$(nproc)
 
 echo '###################################################################'
 echo '###################################################################'
-echo '############################ CUDA #################################'
+echo '############################ xmCUDA #################################'
 echo '###################################################################'
 echo '###################################################################'
 
